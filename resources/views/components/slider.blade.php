4d43fd71e01c010c26feb44b285078208698d459
marcosborg <m.borges.mail@gmail.com>
2024-02-12 17:18:48 +0000

    Update

--- resources/views/components/slider.blade.php
+++ resources/views/components/slider.blade.php
@@ -5,8 +5,7 @@
         <div class="row">
             <div class="col-md-8">
                 <span class="h6 d-inline-block mb-4 subhead text-uppercase">Gym fitness club</span>
-                <h1 class="text-uppercase text-white mb-5">Step up your <span class="text-color">fitness
-                        Challange</span><br>with us</h1>
+                <h1 class="text-uppercase text-white mb-5">Treine como se  <span class="text-color">estivesse em casa</span><br>no seu ginásio privado</h1>
 
                 <a href="pricing.html" target="_blank" class="btn btn-main ">Join Us <i
                         class="ti-angle-right ml-3"></i></a>
