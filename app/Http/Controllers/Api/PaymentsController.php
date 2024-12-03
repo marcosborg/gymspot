a2d115f1692914971a9e5c20e12a0577a769a9cc
marcosborg <m.borges.mail@gmail.com>
2024-12-03 23:22:24 +0000

    Update

--- app/Http/Controllers/Api/PaymentsController.php
+++ app/Http/Controllers/Api/PaymentsController.php
@@ -129,8 +129,8 @@ class PaymentsController extends Controller
             $rented_slot->keypass = mt_rand(100000, 999999);
             $rented_slot->save();
             // CRIAR PASS
-            $start_date_time = Carbon::parse($rented_slot->start_date_time)->timestamp;
-            $end_date_time = Carbon::parse($rented_slot->end_date_time)->timestamp;
+            $start_date_time = Carbon::parse($rented_slot->start_date_time)->addHour()->timestamp;
+            $end_date_time = Carbon::parse($rented_slot->end_date_time)->addHour()->timestamp;
             $this->sendKeycode($rented_slot->keypass, $rented_slot->id, $start_date_time, $end_date_time);
         }
 
