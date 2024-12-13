299e549ef9280d079d0a2ab2ddabf89d80421311
marcosborg <m.borges.mail@gmail.com>
2024-12-13 11:09:02 +0000

    Update

--- app/Http/Middleware/RedirectToAppStore.php
+++ app/Http/Middleware/RedirectToAppStore.php
@@ -4,27 +4,21 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class RedirectToAppStore
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next)
     {
         $userAgent = $request->header('User-Agent');
 
         // Verifica se é um dispositivo iOS
         if (stripos($userAgent, 'iPhone') !== false || stripos($userAgent, 'iPad') !== false) {
-            return redirect('https://apps.apple.com/pt/app/gymspot/id6479336982');
+            return redirect()->away('itms-apps://apps.apple.com/pt/app/gymspot/id6479336982');
         }
 
         // Verifica se é um dispositivo Android
         if (stripos($userAgent, 'Android') !== false) {
-            return redirect('https://play.google.com/store/apps/details?id=pt.gymspot.app&hl=pt');
+            return redirect()->away('market://details?id=com.gymspot.app');
         }
 
         // Caso não seja um dispositivo mobile, prosseguir normalmente
