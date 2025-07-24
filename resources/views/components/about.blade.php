451a6f2ede80fc2975535ad1b0eba250d701784e
marcosborg <m.borges.mail@gmail.com>
2025-07-24 11:15:50 +0100

    Update call and about components to link directly to Google Play and App Store

--- resources/views/components/about.blade.php
+++ resources/views/components/about.blade.php
@@ -5,16 +5,15 @@
     <div class="container">
         <div class="row align-items-center">
             <div class="col-lg-6">
-                <img src="{{ $about->image ? $about->image->getUrl() : 'assets/website/images/bg/bg-5.jpg' }}" alt=""
-                    class="img-fluid rounded shadow w-100">
+                <img src="{{ $about->image ? $about->image->getUrl() : 'assets/website/images/bg/bg-5.jpg' }}" alt="" class="img-fluid rounded shadow w-100">
             </div>
 
             <div class="col-lg-6">
                 <div class="pl-3 mt-5 mt-lg-0">
                     <h2 class="mt-1 mb-3">{{ $about->title }}</h2>
                     <p class="mb-4 text-justify">{{ $about->text }}</p>
-                    <a href="/{{ $about->link }}" class="btn btn-main">{{ $about->button }}<i
-                            class="fa fa-angle-right ml-2"></i></a>
+                    <a href="https://play.google.com/store/apps/details?id=pt.gymspot.app" class="btn btn-main ">Google Play <i class="ti-angle-right ml-3"></i></a>
+                    <a href="https://apps.apple.com/pt/app/gymspot/id6479336982" class="btn btn-main ">App Store <i class="ti-angle-right ml-3"></i></a>
                 </div>
             </div>
         </div>
--- resources/views/components/call.blade.php
+++ resources/views/components/call.blade.php
@@ -7,7 +7,8 @@
                 <div class="text-center">
                     <h2 class="text-lg mt-4 mb-3 text-white">{{ $call->title }}</h2>
                     <span class="h3 letter-spacing text-white">{{ $call->subtitle }}</span><br>
-                    <a href="{{ $call->link }}" class="btn btn-main text-white mt-4">{{ $call->button }}</a><br>
+                    <a href="https://play.google.com/store/apps/details?id=pt.gymspot.app" class="btn btn-main text-white mt-4">Google Play</a>
+                    <a href="https://apps.apple.com/pt/app/gymspot/id6479336982" class="btn btn-main text-white mt-4">App Store</a><br>
                     <img src="/assets/website/images/logos-stores.png" width="450" class="mt-4 img-responsive">
                 </div>
             </div>
